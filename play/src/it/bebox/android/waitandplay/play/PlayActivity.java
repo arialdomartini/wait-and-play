45b22878463a3066fcacac6a4c9c0cdc72f49e8a
Arialdo Martini <arialdomartini@gmail.com>
2012-07-03 23:41:56 +0200

    Empty android project with empty test project

---
+++ play/src/it/bebox/android/waitandplay/play/PlayActivity.java
@@ -0,0 +1,13 @@
+package it.bebox.android.waitandplay.play;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PlayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
